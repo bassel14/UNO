08b803871e9ac56bfe167f0d5e6b8ea258886f41
bassel14 <github@webgearmedia.com>
2018-09-13 18:08:29 +0300

    Add files via upload

--- template-home-school.php
+++ template-home-school.php
@@ -47,30 +47,34 @@ get_header();
                     <div class="container">
                         <div class="row">
 
-                            <div class="col-sm-4 varity-wraper">  
-                            <h2>Music For All Ages</h2>                     
-                            <div class="componont-icon">
-                                <i class="fas fa-music"></i>
-                            </div>
-                            <p>The creative music, movement, and dramatics program is an on-going flexible process that integrates itself into the academic program of the International Montessori School.</p>
-                            </div> <!-- end varity wrap -->
+                            <main class="about-componont-type2">
                             
-                            <div class="col-sm-4 varity-wraper">   
-                                <h2>Sports for All Ages</h2>                    
-                                <div class="componont-icon">   
-                                    <i class="fas fa-swimmer"></i>
-                                </div>
-                                <p>Dr. Montessori believed that this the beginning of conscious. It is brought about by the intelligence working in a concentrated way on the impressions given by the senses.</p>
-                            </div> <!-- end varity wrap -->
+                                <div class="componont-item-type2">               
+                                    <div class="componont-icon">
+                                        <i class="fas fa-music"></i>
+                                    </div>                                
+                                    <h2>Music for All Ages</h2> 
+                                    <p>The creative music, movement, and dramatics program is an on-going flexible process that integrates itself into the academic program of the International Montessori School.</p>     
+                                </div> <!-- end item  -->
+                                
+                                <div class="componont-item-type2">                       
+                                    <div class="componont-icon">   
+                                        <i class="fas fa-swimmer"></i>
+                                    </div>                            
+                                    <h2>Sports for All Ages</h2> 
+                                    <p>Dr. Montessori believed that this the beginning of conscious. It is brought about by the intelligence working in a concentrated way on the impressions given by the senses.</p>
+                                </div> <!-- end item  -->
+                                
+                                <div class="componont-item-type2">               
+                                    <div class="componont-icon">
+                                        <i class="fas fa-user-graduate"></i>
+                                    </div>    
+                                    <h2>Language for All Ages</h2>  
+                                    <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
+                                        him or her conscious awareness of language in order that it may be used more effectively.</p>
+                                </div> <!-- end item  -->
 
-                            <div class="col-sm-4 varity-wraper">     
-                                <h2>Language for All Ages</h2>                  
-                                <div class="componont-icon">
-                                    <i class="fas fa-user-graduate"></i>
-                                </div>
-                                <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
-                                    him or her conscious awareness of language in order that it may be used more effectively.</p>
-                            </div> <!-- end varity wrap -->
+                            </main>
 
                         </div> <!-- end row -->
                     </div> <!-- end container -->
@@ -80,6 +84,54 @@ get_header();
         </div><!-- End about-container -->
        
     </section> <!-- End of About us Section -->
+
+
+    <section class="programs-section">
+
+        <div class="section Title">
+            <h2>Our Programs</h2>
+        </div>
+
+        <main class="about-componont-type2">
+                            
+            <div class="componont-item-type2">               
+                <div class="componont-icon">
+                    <i class="fas fa-music"></i>
+                </div>                                
+                <h2>Music for All Ages</h2> 
+                <p>The creative music, movement, and dramatics program is an on-going flexible process that integrates itself into the academic program of the International Montessori School.</p>     
+            </div> <!-- end item  -->
+            
+            <div class="componont-item-type2">                       
+                <div class="componont-icon">   
+                    <i class="fas fa-swimmer"></i>
+                </div>                            
+                <h2>Sports for All Ages</h2> 
+                <p>Dr. Montessori believed that this the beginning of conscious. It is brought about by the intelligence working in a concentrated way on the impressions given by the senses.</p>
+            </div> <!-- end item  -->
+            
+            <div class="componont-item-type2">               
+                <div class="componont-icon">
+                    <i class="fas fa-user-graduate"></i>
+                </div>    
+                <h2>Language for All Ages</h2>  
+                <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
+                    him or her conscious awareness of language in order that it may be used more effectively.</p>
+            </div> <!-- end item  -->
+            
+            <div class="componont-item-type2">               
+                <div class="componont-icon">
+                    <i class="fas fa-user-graduate"></i>
+                </div>    
+                <h2>Language for All Ages</h2>  
+                <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
+                    him or her conscious awareness of language in order that it may be used more effectively.</p>
+            </div> <!-- end item  -->
+        </main>
+
+
+
+    </section> <!-- End programs-section -->
 
     <section class="new-events-section">
 
@@ -113,7 +165,7 @@ get_header();
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/post1.jpg" alt="first image" />
                                 </div>                                
                                 <div class="cards-desc-container extra-top-margin card-color-bg">
-                                    <h2>Mathmatic and Phisics class </h2>
+                                    <h2><a href="#">Mathmatic and Phisics class</a></h2>
                                     <div class="card-date"><span class="tribe-event-date-start">Septemper 10, 2018</span></div>  
                                     <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
                                 </div> 
@@ -126,7 +178,7 @@ get_header();
                                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/post2.jpg" alt="first image" />
                                 </div>                                
                                 <div class="cards-desc-container extra-top-margin card-color-bg">
-                                    <h2>Haidi and friends return to School </h2>
+                                    <h2><a href="#">Haidi and friends return to School</a></h2>
                                     <div class="card-date"><span class="tribe-event-date-start">Septemper 8, 2018</span></div>  
                                     <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
                                 </div> 
@@ -137,20 +189,20 @@ get_header();
                             <div class="card-wrapper extra-car-top-margin card-color-bg-no-margin"> 
                                 <div class="card-title">
                                     <h2>Events Calendar</h2>
-                                    <p>See all upcoming events</p>
+                                    <a href="#">See all upcoming events</a>
                                 </div>
                                 <div class="cards-desc-container">
                                     <ul class="cards-list">
                                         <li>
-                                            <div class="card-date"><span class="tribe-event-date-start">February 8, 2019</span></div>    
-                                            <div class="card-info">Board Meeting »</div>
+                                            <div class="card-date"><span class="tribe-event-date-start">February 8, 2018</span></div>    
+                                            <div class="card-info"><a href="#">Board Meeting »</a></div>
                                         </li>
                                         <li>
                                             <div class="card-date"><span class="tribe-event-date-start">February 8, 2019</span></div>    
-                                            <div class="card-info">Most part fantastic faculty members for students »</div></li>
+                                            <div class="card-info"><a href="#">Most part fantastic faculty members for students »</a></div></li>
                                         <li>
                                             <div class="card-date"><span class="tribe-event-date-start">February 8, 2019</span></div>    
-                                            <div class="card-info">Advice: The future of Startups without Support »</div>
+                                            <div class="card-info"><a href="#">Advice: The future of Startups without Support »</a></div>
                                         </li>
                                     </ul>
                                 </div>  
