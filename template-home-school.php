393e70d6d3d5e0fed20c7645284dcb2632e97935
bassel14 <github@webgearmedia.com>
2018-09-16 13:08:57 +0300

    Add files via upload

--- template-home-school.php
+++ template-home-school.php
@@ -88,50 +88,177 @@ get_header();
 
     <section class="programs-section">
 
-        <div class="section Title">
-            <h2>Our Programs</h2>
-        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section-header">
+                        <div class="section-title">
+                            <h2>Our Programs</h2>
+                        </div>
+                        <div class="section-sub-title">
+                            <p>Toddlers, Pre-School & Elementry programs</p>
+                        </div>
+                    </div>
+                </div><!--  End Col12 -->
+            </div> <!-- End row -->
+            
+        </div> <!-- End container -->
 
-        <main class="about-componont-type2">
-                            
-            <div class="componont-item-type2">               
-                <div class="componont-icon">
-                    <i class="fas fa-music"></i>
-                </div>                                
-                <h2>Music for All Ages</h2> 
-                <p>The creative music, movement, and dramatics program is an on-going flexible process that integrates itself into the academic program of the International Montessori School.</p>     
-            </div> <!-- end item  -->
-            
-            <div class="componont-item-type2">                       
-                <div class="componont-icon">   
-                    <i class="fas fa-swimmer"></i>
-                </div>                            
-                <h2>Sports for All Ages</h2> 
-                <p>Dr. Montessori believed that this the beginning of conscious. It is brought about by the intelligence working in a concentrated way on the impressions given by the senses.</p>
-            </div> <!-- end item  -->
-            
-            <div class="componont-item-type2">               
-                <div class="componont-icon">
-                    <i class="fas fa-user-graduate"></i>
-                </div>    
-                <h2>Language for All Ages</h2>  
-                <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
-                    him or her conscious awareness of language in order that it may be used more effectively.</p>
-            </div> <!-- end item  -->
-            
-            <div class="componont-item-type2">               
-                <div class="componont-icon">
-                    <i class="fas fa-user-graduate"></i>
-                </div>    
-                <h2>Language for All Ages</h2>  
-                <p>Language is an important part of the entire Montessori curriculum. Its treatment as a separate subject comes only at the points in which it is necessary to give clarity to the child's mind – that is, to give
-                    him or her conscious awareness of language in order that it may be used more effectively.</p>
-            </div> <!-- end item  -->
+        <main class="main-cards-type-4">
+
+            <div class="card-1">
+                <div class="card-wrapper no-top-margin"> 
+                    <div class="cards-img-container">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/toddler_img.jpg" alt="first image" />
+                    </div>                                
+                    <div class="cards-desc-container">
+                        <h2><a href="#">Toddlers</a></h2>
+                        <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
+                    </div> 
+                </div>
+            </div> <!-- end of card-1 -->                    
+
+            <div class="card-2">
+                <div class="card-wrapper no-top-margin"> 
+                    <div class="cards-img-container">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pre-school.jpg" alt="second image" />
+                    </div>                                
+                    <div class="cards-desc-container">
+                        <h2><a href="#">PRESCHOOLERS</a></h2>
+                        <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
+                    </div> 
+                </div>
+            </div> <!-- end of card-2 -->                   
+
+            <div class="card-3">
+                <div class="card-wrapper no-top-margin"> 
+                    <div class="cards-img-container">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/pre-k.jpg" alt="third image" />
+                    </div>                                
+                    <div class="cards-desc-container">
+                        <h2><a href="#">PRE-K</a></h2>
+                        <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
+                    </div> 
+                </div>
+            </div> <!-- end of card-2 -->                   
+
+            <div class="card-4">
+                <div class="card-wrapper no-top-margin"> 
+                    <div class="cards-img-container">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/elementry.jpg" alt="fourth image" />
+                    </div>                                
+                    <div class="cards-desc-container">
+                        <h2><a href="#">Elementry</a></h2>
+                        <p>Lommodo ligula eget dolor. Aenean massa. Cum sociis que penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec…</p>
+                    </div> 
+                </div>
+            </div> <!-- end of card-2 --> 
+
         </main>
 
 
+        <main class="main-cards-type-3">
+            
+            <div class="card-1 card-type2">
+                <div class="card-wrapper"> 
+                    <div class="card-title">
+                        <h2>School Announcements</h2>
+                    </div>
+                    <div class="cards-desc-container">
+                        <ul class="cards-list type-colored  color-red">
+                            <li>
+                                <span class="tribe-event-date-start">February 8, 2018</span>    
+                                <span class="text-right"><a href="#">Parents Meetings</a></span>   
+                            </li>
+                            <li>
+                                <span class="tribe-event-date-start">February 8, 2018</span>    
+                                <span class="text-right"><a href="#">first payment close up</a></span>   
+                            </li>
+                            <li>
+                                <span class="tribe-event-date-start">February 8, 2018</span>    
+                                <span class="text-right"><a href="#">The second meeting </a></span>   
+                            </li>
+                            <li>
+                                <span class="tribe-event-date-start">February 8, 2018</span>    
+                                <span class="text-right"><a href="#">second payment close up</a></span>   
+                            </li>
+                            <li>
+                                <div class="card-info"><a href="#">Provides opportunities for the child to explore »</a></div>
+                            </li>
+                        </ul>
+                    </div>  
+                </div>
+            </div> <!-- end of card-3 -->  
 
-    </section> <!-- End programs-section -->
+            <div class="card-2 card-type2">
+                <div class="card-wrapper"> 
+                    <div class="card-title">
+                        <h2>Working hours</h2>
+                    </div>
+                    <div class="cards-desc-container">
+                        <ul class="cards-list type-colored color-green">
+                            <li>
+                                <span class="text-left">8.00 - 2.00</span>
+                                <span class="text-right">Sun-Wed</span>   
+                            </li>
+                            <li>
+                                <span class="text-left">8.00 - 12.00</span>
+                                <span class="text-right">Thu</span>   
+                            </li>
+                            <li>
+                                <span class="text-left">10.00 - 2.00</span>
+                                <span class="text-right">Sat</span>   
+                            </li>
+                            <li>
+                                <span class="text-left">10:40 - 11:20</span>
+                                <span class="text-right">Holidays </span>
+                            </li>
+                            <li>
+                                <div class="card-info"><a href="#">Provides opportunities for the child to explore »</a></div>
+                            </li>
+                        </ul>
+                    </div>  
+                </div>
+            </div> <!-- end of card-3 -->
+
+            <div class="card-3 card-type2">
+                <div class="card-wrapper"> 
+                    <div class="card-title">
+                        <h2>DAILY PROGRAMS</h2>
+                    </div>
+                    <div class="cards-desc-container">
+                        <ul class="cards-list type-colored  color-blue">
+                            <li>
+                                <span class="text-left">7.00 - 7.30</span>   
+                                <span class="text-right">Flag Rising</span>
+                            </li>
+                            <li>
+                                <span class="text-left">8.00 - 9.30</span>   
+                                <span class="text-right">First Lesson</span>
+                            </li>
+                            <li>
+                                <span class="text-left">9.30 - 10.00</span>   
+                                <span class="text-right">First Snack</span>
+                            </li>
+                            <li>
+                                <span class="text-left">10.00 - 10.30</span>
+                                <span class="text-right">Outdoor Play</span>
+                            </li>
+                            <li>
+                                <div class="card-info"><a href="#">Provides opportunities for the child to explore »</a></div>
+                            </li>
+                        </ul>
+                    </div>  
+                </div>
+            </div> <!-- end of card-3 -->
+
+        </main>
+
+        <div class="spacer-content"></div>
+
+        </section> <!-- End programs-section -->
+
+
 
     <section class="new-events-section">
 
@@ -149,7 +276,7 @@ get_header();
                         </div>
                     </div>
                 </div>
-            </div>>
+            </div>
 
         </div>  <!-- End img-bg -->
 
